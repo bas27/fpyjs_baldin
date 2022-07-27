a9b9aef8c49613ecebb48ec0dfa2105099fd358a
Alexander Baldin <bas2784@gmail.com>
2022-07-27 14:50:55 +0300

    Добавлена остановка таймера

--- Interface Design Basics/js-features/countdown/task.js
+++ Interface Design Basics/js-features/countdown/task.js
@@ -1,8 +1,15 @@
 const timer = document.getElementById("timer")
+var t = setInterval(start, 1000);
 
-setInterval(() => {
-    timer.textContent = Number(timer.textContent) - 1;
-    if (timer.textContent == 0) {
-        alert('Вы победили в конкурсе');
-    };
-}, 1000)
+function start () {
+    if (timer.textContent <= 0) {
+        clearTimeout(t);
+    }
+    else {
+        timer.textContent = Number(timer.textContent) - 1;
+        if (timer.textContent == 0) {
+            alert('Вы победили в конкурсе');
+        }
+
+    }
+}
