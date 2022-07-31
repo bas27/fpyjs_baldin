b0ef055488b820460f9722a78f12ec6e95f7db0f
Alexander B <bas2784@gmail.com>
2022-07-31 22:21:03 +0300

    без функции

--- Interface Design Basics/js-features/mole-game/task.js
+++ Interface Design Basics/js-features/mole-game/task.js
@@ -1,15 +1,37 @@
 const dead = document.getElementById('dead')
 const lost = document.getElementById('lost')
 
-function getHole(index) {
-    document.getElementById(`hole${index}`)
-}
+// function getHole(index) {
+//     document.getElementById(`hole${index}`)
+// }
 
-for (let i = 1; i < 10; i++) {
-    if (getHole(i).onclick == 'hole hole_has-mole') {
-        dead.textContent += 1;
+
+
+
+// for (let i = 1; i < 10; i++) {
+//     if (getHole(i).onclick == 'hole hole_has-mole') {
+//         dead.textContent += 1;
+//     }
+//     else {
+//         lost.textContent += 1;
+//     }
+// }
+
+
+var hole1 = document.getElementById('hole1')
+
+hole1.onclick = () => {
+    if (hole1.className.includes('hole_has-mole')) {
+        dead.textContent = Number(dead.textContent) + 1;
     }
     else {
-        lost.textContent += 1;
+        lost.textContent = Number(lost.textContent) + 1;
+    }
+}
+
+function getHole() {
+for (let index = 0; index < 10; index++) {
+    if (document.getElementById(`hole${index}`).onclick == `hole${index}`) {
+        return `hole${index}`
     }
 }
