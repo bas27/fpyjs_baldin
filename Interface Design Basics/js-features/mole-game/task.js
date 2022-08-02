1dfade683e0e2c916e323cb5a5efd5a693f7f184
Alexander B <bas2784@gmail.com>
2022-08-02 11:46:13 +0300

    Mole code upgrade ;)

--- Interface Design Basics/js-features/mole-game/task.js
+++ Interface Design Basics/js-features/mole-game/task.js
@@ -1,28 +1,37 @@
-const dead = document.getElementById('dead');
-const lost = document.getElementById('lost');
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+const arr = document.getElementsByClassName('hole')
 
-var getHole = index => document.getElementById(`hole${index}`);
+const getHole = (index) => document.getElementById(`hole${index}`);
 
+function reset() {
+  dead.textContent = 0;
+  lost.textContent = 0;
+}
 
-for (let index = 1; index < 10; index++) {
-    let hole = getHole(index);
-    hole.addEventListener('click', () => {
-        if (hole.className.includes('hole_has-mole')) {
-            dead.textContent++;
-            if (Number(dead.textContent) == 10) {
-                alert("Победа!");
-                dead.textContent = 0;
-                lost.textContent = 0;
-            }
-        }
-        else {
-            lost.textContent++;
-            if (Number(lost.textContent) == 5) {
-                alert("Вы проиграли");
-                dead.textContent = 0;
-                lost.textContent = 0;
-            }
+function myAlert(event) {
+  if (event == dead) {
+    alert("Победа!");
+  }
+  if (event == lost) {
+    alert("Вы проиграли");
+  }
+}
 
-        }
-    });
-};
+for (let index = 1; index < arr.length + 1; index++) {
+  let hole = getHole(index);
+
+  hole.addEventListener("click", () => {
+    hole.className.includes("hole_has-mole") ? dead.textContent++ : lost.textContent++;
+
+    if (dead.textContent == 10) {
+      myAlert(dead);
+      reset();
+    }
+
+    if (lost.textContent == 5) {
+      myAlert(lost);
+      reset();
+    }
+  });
+}
