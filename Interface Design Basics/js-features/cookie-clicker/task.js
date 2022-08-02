587d5329ee5eca10f604079faf6d14a59aac37a7
Alexander B <bas2784@gmail.com>
2022-08-02 13:12:20 +0300

    Попоравил объявление переменных

--- Interface Design Basics/js-features/cookie-clicker/task.js
+++ Interface Design Basics/js-features/cookie-clicker/task.js
@@ -1,12 +1,12 @@
 const clickerCounter = document.getElementById('clicker__counter');
 const img = document.getElementById('cookie');
 
-var timeClick = document.getElementById('clicker__time');
-var lastClicked = 0;
+let timeClick = document.getElementById('clicker__time');
+let lastClicked = 0;
 
 img.onmousedown = () => {
     img.width = Number(img.width) + 20;
-    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
+    clickerCounter.textContent++
     var timeNow =  (new Date()).getTime();
     timeClick.textContent = (1/((timeNow - lastClicked)/1000)).toFixed(2);
     lastClicked = timeNow;
@@ -16,4 +16,8 @@ img.onmouseup = () => {
         img.width = Number(img.width) - 20;
     }
 
-
+//второй вариант
+// img.onclick = () => {
+//     img.width = ++clickerCounter.textContent % 2 ? 250 : 200;
+//     ...
+// }
