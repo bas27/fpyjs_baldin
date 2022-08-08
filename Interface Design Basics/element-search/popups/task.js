bfb6d83770b7f810199b06f693aeea99bfcec16e
Alexander B <bas2784@gmail.com>
2022-08-08 09:57:37 +0300

    закрытие окна реализовано через класс

--- Interface Design Basics/element-search/popups/task.js
+++ Interface Design Basics/element-search/popups/task.js
@@ -4,14 +4,17 @@ const modalSuccess = document.getElementById("modal_success");
 const btnShSuccess = document.querySelector(".show-success");
 const close = document.getElementsByClassName("modal__close_times");
 
+
 btnShSuccess.addEventListener("click", function () {
   main.classList.remove("modal_active");
   modalSuccess.classList.add("modal_active");
 });
 
+
 for (let i = 0, len = close.length; i < len; i++) {
-  close[i].onclick = () => {
-    main.style.display = "none";
-    modalSuccess.style.display = "none";
-  };
-}
+    close[i].addEventListener('click', function() {
+      let modalActive = document.querySelector('.modal_active');
+      modalActive.classList.remove("modal_active");
+    });
+
+ };
