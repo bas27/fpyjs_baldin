fe83dcc4bd1426efc4dd54935da968e162677e1d
Alexander B <bas2784@gmail.com>
2022-08-09 13:41:02 +0300

    Открытие и закрытие меню

--- Interface Design Basics/element-search/menu/task.js
+++ Interface Design Basics/element-search/menu/task.js
@@ -2,16 +2,16 @@ const links = document.querySelectorAll(".menu__link");
 
 for (let i = 0, len = links.length; i < len; i++) {
   let link = links[i];
+
   link.addEventListener("click", function (e) {
-    let active = document.querySelector(".menu_active");
-    if (active) {
-      active.classList.remove("menu_active");
-    }
     let nextSibling = e.target.nextElementSibling;
 
     if (nextSibling) {
       e.preventDefault();
-      nextSibling.classList.add("menu_active");
+
+      nextSibling.className.includes("menu_active")
+        ? nextSibling.classList.remove("menu_active")
+        : nextSibling.classList.add("menu_active");
     }
   });
 }
