d02d5a66ed91590986350ea4b5f5df4dacf5a22e
Alexander B <bas2784@gmail.com>
2022-08-16 21:29:00 +0300

    ver 0.2

--- Interface Design Basics/event-object/dropdown/task.js
+++ Interface Design Basics/event-object/dropdown/task.js
@@ -1,24 +1,21 @@
 const dropdownList = document.querySelector('.dropdown__list');
-const dropdownValue = document.querySelector('.dropdown__value')
-const dropdownItem = document.querySelectorAll('li > a')
-// const arItem = Array.from(dropdownItem)
-
-dropdownValue.addEventListener("click", () => {
-  dropdownList.className.includes("dropdown__list_active")
-    ? dropdownList.classList.remove("dropdown__list_active")
-    : dropdownList.classList.add("dropdown__list_active");
-
-
-});
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownItem = document.querySelectorAll('li > a');
 
 
 for (let i = 0, len = dropdownItem.length; i < len; i++) {
     curLink = dropdownItem[i]
     curLink.addEventListener('click', function(e) {
-      console.log(dropdownValue.textContent)
-      
-      // dropdownValue.textContent = e.textContent
-      dropdownList.classList.remove('dropdown__list_active')
-      e.stopPropagation()
+            console.log(this.textContent)
+            if (this.textContent != dropdownValue.textContent) {
+                dropdownValue.textContent = this.textContent
+            }
+            e.stopPropagation();
         })
 }
+
+dropdownValue.addEventListener("click", () => {
+  dropdownList.className.includes("dropdown__list_active")
+    ? dropdownList.classList.remove("dropdown__list_active")
+    : dropdownList.classList.add("dropdown__list_active");
+});
