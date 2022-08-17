c1cb0d5aa75c0d5a1d660d9d156e9d28755542cc
Alexander B <bas2784@gmail.com>
2022-08-17 11:39:01 +0300

    Релиз))

--- Interface Design Basics/event-object/dropdown/task.js
+++ Interface Design Basics/event-object/dropdown/task.js
@@ -2,20 +2,17 @@ const dropdownList = document.querySelector('.dropdown__list');
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownItem = document.querySelectorAll('li > a');
 
-
-for (let i = 0, len = dropdownItem.length; i < len; i++) {
-    curLink = dropdownItem[i]
-    curLink.addEventListener('click', function(e) {
-            console.log(this.textContent)
-            if (this.textContent != dropdownValue.textContent) {
-                dropdownValue.textContent = this.textContent
-            }
-            e.stopPropagation();
-        })
-}
-
 dropdownValue.addEventListener("click", () => {
   dropdownList.className.includes("dropdown__list_active")
     ? dropdownList.classList.remove("dropdown__list_active")
     : dropdownList.classList.add("dropdown__list_active");
 });
+
+for (let link of dropdownItem) {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        dropdownValue.textContent = e.target.textContent;
+        dropdownList.classList.remove("dropdown__list_active")
+    })
+}
+
