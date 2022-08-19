e0a7752fdfbc5831128045e4984f810f35a393ff
Alexander B <bas2784@gmail.com>
2022-08-19 10:28:12 +0300

    управление вкладками. финальная версия.

--- Interface Design Basics/event-object/tabs/task.js
+++ Interface Design Basics/event-object/tabs/task.js
@@ -1,32 +1,28 @@
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.tab__content');
 const ar_tabs = toArray(tabs);
-const ar_contents = toArray(contents);
-
-console.log(ar_tabs);
-console.log(ar_contents);
 
 for (let tab of ar_tabs) {
-    
-    tab.addEventListener('click', (e) => {
-        console.log(e.target.classList)
-        if (!e.target.className.includes('tab_active')) {
-            let i = ar_tabs.indexOf(tab)
-            console.log(i)
-            console.log('Привет')
-            let active = document.querySelector('.tab_active')
-            let activeCont = document.querySelector('.tab__content_active')
-            active.classList.remove('tab_active')
-            activeCont.classList.remove('tab__content_active')
-            e.target.classList.add('tab_active')
-            console.log(ar_contents[i])
-        }
-    })
+  tab.addEventListener("click", (e) => {
+    console.log(e.target.classList);
+    if (!e.target.className.includes("tab_active")) {
+      let i = ar_tabs.indexOf(tab);
+
+      let active = document.querySelector(".tab_active");
+      let activeCont = document.querySelector(".tab__content_active");
+
+      active.classList.remove("tab_active");
+      activeCont.classList.remove("tab__content_active");
+
+      e.target.classList.add("tab_active");
+      contents[i].classList.add("tab__content_active");
+    }
+  });
 }
 
-function toArray(coll){
-    for (var i = 0, a = [], len = coll.length; i < len; i++) {
+function toArray(coll) {
+  for (var i = 0, a = [], len = coll.length; i < len; i++) {
     a[i] = coll[i];
-    }
-   return a;
-    }
+  }
+  return a;
+}
