eece3a550955d446ae4a283044efe9872e7b88bc
Alexander B <bas2784@gmail.com>
2022-08-25 14:07:45 +0300

    появление элементов при прокрутке

--- Interface Design Basics/dom/reveal/task.js
+++ Interface Design Basics/dom/reveal/task.js
@@ -1,10 +1,24 @@
-const reveal = document.querySelectorAll('.reveal')
+let last_known_scroll_position = 0;
+let ticking = false;
 
-window.addEventListener('scroll', isVisisble(reveal));
+function visible(scroll_pos) {
+  const reveal = document.querySelectorAll(".reveal");
+  for (let rev of reveal) {
+    const { top, bottom } = rev.getBoundingClientRect();
+    top < 0 || bottom > window.innerHeight
+      ? rev.classList.remove("reveal_active")
+      : rev.classList.add("reveal_active");
+  }
+}
+window.addEventListener("scroll", (e) => {
+  last_known_scroll_position = window.scrollY;
 
-  function isVisisble(els) {
-    for (let el of els) {
-        const { top, bottom } = el.getBoundingClientRect()
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      visible(last_known_scroll_position);
+      ticking = false;
+    });
 
-        el.classList.add('reveal_active')
-  }}
+    ticking = true;
+  }
+});
